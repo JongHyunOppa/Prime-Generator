66612f72eff57a362484a57a31beff0d86f4876f
Amina Faradzhieva <34938706+JongHyunOppa@users.noreply.github.com>
2018-11-26 23:53:45 +0600

    Add files via upload

---
+++ prime_generator.py
@@ -0,0 +1,35 @@
+def primes_sieve(limit):
+    limitn = limit+1
+    primes = range(2, limitn)
+
+    for i in primes:
+        factors = range(i, limitn, i)
+        print factors
+        for f in factors[1:]:
+            if f in primes:
+                primes.remove(f)
+    return primes
+print primes_sieve(17)
+'''
+def rwh_primes(n):
+    sieve = [True] * n
+    for i in xrange(3,int(n**0.5)+1,2):
+        if sieve[i]:
+            sieve[i*i::2*i]=[False]*((n-i*i-1)/(2*i)+1)
+    return [2] + [i for i in xrange(3,n,2) if sieve[i]]
+
+print rwh_primes(1000)'''
+
+'''def rwh_primes2(n):
+    correction = (n%6>1)
+    n = {0:n,1:n-1,2:n+4,3:n+3,4:n+2,5:n+1}[n%6]
+    sieve = [True] * (n/3)
+    sieve[0] = False
+    for i in xrange(int(n**0.5)/3+1):
+      if sieve[i]:
+        k=3*i+1|1
+        sieve[      ((k*k)/3)      ::2*k]=[False]*((n/6-(k*k)/6-1)/k+1)
+        sieve[(k*k+4*k-2*k*(i&1))/3::2*k]=[False]*((n/6-(k*k+4*k-2*k*(i&1))/6-1)/k+1)
+    return [2,3] + [3*i+1|1 for i in xrange(1,n/3-correction) if sieve[i]]
+
+print rwh_primes2(1000000)'''
